98b56c2cd138428f7ba26b6ec191879c89437025
vdelamazah <vdelamazah@gmail.com>
2016-12-02 23:57:27 -0300

    arreglando devise

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,8 @@
 class Client < ApplicationRecord
-	before_action :authenticate_user!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  
 end
