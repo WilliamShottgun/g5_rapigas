370a25c736ebceeac85128b37c960434c98c7b94
vdelamazah <vdelamazah@gmail.com>
2016-12-22 19:37:04 -0300

    agregando cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_client!
+
+  # def current_ability
+  # 	@current_ability ||= case 
+  # 											 when current_user
+  # 											 	UserAbility.new(current_user)
+  # 											 when current_client
+  # 											 	ClientAbility.new(current_client)
+  # 											 end
+  # end
 end
